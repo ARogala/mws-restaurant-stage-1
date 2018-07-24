aef5189723206d7589178a80e5bdae07a5e25618
Andrew Rogala <adrogala87@gmail.com>
2018-07-24 11:11:15 -0400

    add service worker

---
+++ sw_registration.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2018-07-24 07:17:29
+ * @version $Id$
+ */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    	console.log('ServiceWorker registration successful with scope: ', reg.scope);
+    }).catch(function(err) {
+    	console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
